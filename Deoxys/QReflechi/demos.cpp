0edac4365757c3b58fee8ea6489effcfd6fe527e
Adrien Chardon <adrienchardon@mailoo.org>
2017-01-19 22:39:57 +0100

    Edit demos.cpp

--- Deoxys/QReflechi/demos.cpp
+++ Deoxys/QReflechi/demos.cpp
@@ -4,32 +4,33 @@
 int demo_1(OrdersFIFO *oc)
 {
     #define EX_DIST 1000
+    #define DEFAULT_DELAY 2
 
     int e = 0;
 
     e += oc->push(OrderCom_makeAbsPos(EX_DIST,    0));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
             e += oc->push(OrderCom_makeAbsAngle(DEG2RAD(90)));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
     e += oc->push(OrderCom_makeAbsPos(EX_DIST,    EX_DIST));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
             e += oc->push(OrderCom_makeAbsAngle(DEG2RAD(180)));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
     e += oc->push(OrderCom_makeAbsPos(200,        EX_DIST));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
             e += oc->push(OrderCom_makeAbsAngle(DEG2RAD(90)));
             e += oc->push(OrderCom_makeAbsAngle(DEG2RAD(0)));
             e += oc->push(OrderCom_makeAbsAngle(DEG2RAD(270)));
             e += oc->push(OrderCom_makeAbsAngle(DEG2RAD(180)));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
     e += oc->push(OrderCom_makeAbsPos(EX_DIST,    EX_DIST));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
             e += oc->push(OrderCom_makeAbsAngle(DEG2RAD(270)));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
     e += oc->push(OrderCom_makeAbsPos(EX_DIST,    0));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
             e += oc->push(OrderCom_makeAbsAngle(DEG2RAD(0)));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
     e += oc->push(OrderCom_makeAbsPos(0,          0));
 
     return e;
@@ -43,28 +44,28 @@ int demo_2(OrdersFIFO *oc)
     int e = 0;
 
     e += oc->push(OrderCom_makeRelDist(EX_DIST));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
             e += oc->push(OrderCom_makeRelAngle(DEG2RAD(90)));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
     e += oc->push(OrderCom_makeRelDist(EX_DIST));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
             e += oc->push(OrderCom_makeRelAngle(DEG2RAD(90)));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
     e += oc->push(OrderCom_makeRelDist(EX_DIST-200));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
             e += oc->push(OrderCom_makeRelAngle(DEG2RAD(-90)));
             e += oc->push(OrderCom_makeRelAngle(DEG2RAD(-90)));
             e += oc->push(OrderCom_makeRelAngle(DEG2RAD(-90)));
             e += oc->push(OrderCom_makeRelAngle(DEG2RAD(-90)));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
     e += oc->push(OrderCom_makeRelDist(-(EX_DIST-200)));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
             e += oc->push(OrderCom_makeRelAngle(DEG2RAD(90)));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
     e += oc->push(OrderCom_makeRelDist(EX_DIST));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
             e += oc->push(OrderCom_makeRelAngle(DEG2RAD(90)));
-                    e += oc->push(OrderCom_makeDelay(0.500));
+                    e += oc->push(OrderCom_makeDelay(DEFAULT_DELAY));
     e += oc->push(OrderCom_makeRelDist(-EX_DIST));
 
     return e;
