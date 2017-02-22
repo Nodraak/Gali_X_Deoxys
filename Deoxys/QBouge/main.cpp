7184585e019d1d12dfde690c3f22271df5d2d638
Adrien Chardon <adrienchardon@mailoo.org>
2017-02-22 16:08:20 +0100

    MOTHER OF GOD !!

--- Deoxys/QBouge/main.cpp
+++ Deoxys/QBouge/main.cpp
@@ -22,6 +22,28 @@ bool request_next_order = false;
 void asserv_main(void);
 
 
+void interrupt_priorities_init(void)
+{
+    NVIC_SetPriorityGrouping(0);
+
+    NVIC_SetPriority(EXTI0_IRQn, 1);
+    NVIC_SetPriority(EXTI1_IRQn, 1);
+    NVIC_SetPriority(EXTI2_TSC_IRQn, 2);
+    NVIC_SetPriority(EXTI3_IRQn, 2);
+
+    NVIC_SetPriority(TIM2_IRQn, 5); // asserv
+
+    NVIC_SetPriority(CAN_TX_IRQn, 10);
+    NVIC_SetPriority(CAN_RX0_IRQn, 10);
+    NVIC_SetPriority(CAN_RX1_IRQn, 10);
+    NVIC_SetPriority(CAN_SCE_IRQn, 10);
+
+    NVIC_SetPriority(USART1_IRQn, 11);
+    NVIC_SetPriority(USART2_IRQn, 11);
+    NVIC_SetPriority(USART3_IRQn, 11);
+}
+
+
 int main(void)
 {
     Debug *debug = NULL;
@@ -49,6 +71,8 @@ int main(void)
     mc = new MotionController;
     asserv_ticker = new Ticker;
     asserv_ticker->attach(asserv_main, ASSERV_DELAY);
+
+    interrupt_priorities_init();
 
     mem_stats_dynamic(debug);
 
