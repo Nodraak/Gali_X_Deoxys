80b9e6cf66d56fb0ba494b248afd6449ed383a34
Adrien Chardon <adrienchardon@mailoo.org>
2017-07-12 19:01:22 +0200

    [Docker] Update build.sh

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,31 +1,36 @@
 #!/usr/bin/env bash
 
-cp --archive /opt/Deoxys/* /tmp/build/
+set -e
 
-prepare_build() {
+# Arg1: folder QBouge QReflechi or QEntreQSort
+# Arg2: board L432KC or L476RG
+build() {
     cd "/tmp/build/$1"
 
+    # clean existing simlinks
     rm -rf libs mbed-os BUILD
+    # create proper working simlinks
     ln -s /opt/mbed-os .
     ln -s /opt/mbed-libs .
     ln -s /opt/Deoxys/Galib .
 
+    # link build folder to host, to gather the binary output and cache the
+    # temporary build objects (.o files)
     mkdir -p "/tmp/build/output/BUILD_$1"
     ln -s "/tmp/build/output/BUILD_$1" BUILD
+
+    make build
+
+    cp "BUILD/NUCLEO_$2/GCC_ARM/out.bin" "/tmp/build/output/$1.hex"
 }
 
-#post_build() {
-#    cd "/tmp/build/$1"
-#    cp -r "/tmp/build/output/BUILD_$1" BUILD
-#}
+main() {
+    cp --archive /opt/Deoxys/* /tmp/build/
 
-prepare_build QBouge
-prepare_build QReflechi
-prepare_build QEntreQSort
+    build QBouge L432KC
+    build QReflechi L432KC
+    build QEntreQSort L476RG
+}
 
-make build
-
-#post_build QBouge
-
-# todo copy .hex to /tmp/buil/output
+main
 
